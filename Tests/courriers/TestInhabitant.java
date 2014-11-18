e40382622299d6570a237eeb1aa4a8f58a88e31f
SieurCartier <gaston.lemaire@etudiant.univ-lille1.fr>
2014-11-18 23:29:07 +0100

    Finished TestInhabitant

--- Tests/courriers/TestInhabitant.java
+++ Tests/courriers/TestInhabitant.java
@@ -37,27 +37,47 @@ public class TestInhabitant {
 		Inhabitant j = new Inhabitant("Arthur", c);
 		SimpleLetter s = new SimpleLetter(i, j, "Coucou");
 		int accountBefore = i.getAccount().getAccount();
-
 		i.postsLetter(s);
-
 		assertEquals(accountBefore - s.getCost(), i.getAccount().getAccount());
+
+		RegisteredLetter<SimpleLetter> RSL = new RegisteredLetter<SimpleLetter>(
+				s);
+		accountBefore = i.getAccount().getAccount();
+		i.postsLetter(RSL);
+		assertEquals(accountBefore - RSL.getCost(), i.getAccount().getAccount());
+
+		UrgentLetter<SimpleLetter> USL = new UrgentLetter<SimpleLetter>(s);
+		accountBefore = i.getAccount().getAccount();
+		i.postsLetter(USL);
+		assertEquals(accountBefore - USL.getCost(), i.getAccount().getAccount());
+
+		PromissaryNote pn = new PromissaryNote(i, j, 10);
+		accountBefore = i.getAccount().getAccount();
+		i.postsLetter(pn);
+		assertEquals(accountBefore - pn.getCost(), i.getAccount().getAccount());
+		accountBefore = accountBefore - pn.getCost();
+		i.getCity().distributeLetters();
+
+		assertEquals(accountBefore - pn.getContent().getValue().intValue(), i
+				.getAccount().getAccount());
+
 	}
 
 	@Test
 	public void TestReceiveLetter() {
-		/*
+
 		City c = new City("BernardTown");
 		Inhabitant i = new Inhabitant("Bernard", c);
 		Inhabitant j = new Inhabitant("Arthur", c);
-		SimpleLetter s = new SimpleLetter(i, j, "Coucou");
 		PromissaryNote pn = new PromissaryNote(i, j, 10);
-		int accountBefore = i.getAccount().getAccount();
+		int accountBefore = j.getAccount().getAccount();
 
-		i.postsLetter(s);
+		i.postsLetter(pn);
+		c.distributeLetters();
 
-		assertEquals(accountBefore - s.getCost(), i.getAccount().getAccount());
-		*/
-
+		assertEquals(accountBefore + pn.getContent().getValue().intValue() - 1,
+				j.getAccount().getAccount());
+		assertFalse(c.getLetterbox().isEmpty()) ;
 	}
 
 }
