f07c3adf25eb9a755ab2d6f2ad762dc74f52e4af
SieurCartier <gaston.lemaire@etudiant.univ-lille1.fr>
2014-11-18 16:59:42 +0100

    methode action

--- src/courriers/UrgentLetter.java
+++ src/courriers/UrgentLetter.java
@@ -1,10 +1,22 @@
 package courriers;
 
-public class UrgentLetter<T extends Letter<?>> extends AbstractLetterDecorator<T> {
-	
-	
-	
-	
-	
+public class UrgentLetter<T extends Letter<?>> extends
+		AbstractLetterDecorator<T> {
+
+	public UrgentLetter(T letter) {
+		super(letter);
+		// TODO Stub du constructeur généré automatiquement
+	}
+
+	@Override
+	public String toString() {
+		// TODO Stub de la méthode généré automatiquement
+		return null;
+	}
+
+	@Override
+	public void action() {
+		sender.getAccount().debit(2 * content.getCost()) ;
+	}
 
 }
